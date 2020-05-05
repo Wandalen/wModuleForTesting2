7600ffe7795e6c3801ca71f873eafaa72ea0e964
Dmytro <dm.vict.kr@gmail.com>
2020-05-05 12:04:39 +0300

    Change structure, add file `Tools.s`

--- proto/dwtools/abase/l2/testing2/Include.s
+++ proto/dwtools/abase/l2/testing2/Include.s
@@ -9,7 +9,7 @@
 let _ = null;
 
 if( typeof module !== 'undefined' )
-_ = require( './l1/ModuleForTesting2.s' );
+_ = require( './ModuleForTesting2.s' );
 
 if( typeof module !== 'undefined' && module !== null )
 module[ 'exports' ].mulOfNumbers = _.mulOfNumbers;
