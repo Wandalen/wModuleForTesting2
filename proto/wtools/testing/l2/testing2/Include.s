e343c1b09485d84cf5bfd41de9f7a98c3ce961a8
Dmytro <dm.vict.kr@gmail.com>
2020-12-22 09:07:52 +0200

    Use global namespace `_test_` in files of module

--- proto/wtools/testing/l2/testing2/Include.s
+++ proto/wtools/testing/l2/testing2/Include.s
@@ -1,17 +1,19 @@
-( function _Include_s_( ) {
+( function _Include_s_()
+{
 
 'use strict';
 
 /**
  * Relations module.
-  @module Tools/base/ModuleForTesting2
-*/
+ * @module Tools/base/ModuleForTesting2
+ */
+
 let _ = null;
 
 if( typeof module !== 'undefined' )
 _ = require( './ModuleForTesting2.s' );
 
 if( typeof module !== 'undefined' && module !== null )
-module[ 'exports' ].mulOfNumbers = _.mulOfNumbers;
+module[ 'exports' ] = _;
 
 })();
